9777fa8c7c6917fea808ac7bbd28e64e9ae53b79
BrodyBoyd <Brody_Boyd@insideranken.org>
2024-11-08 11:57:19 -0600

    pickem

---
+++ pickem personal project/pickem.js
@@ -0,0 +1,14 @@
+const btnElList = document.querySelectorAll('.btn');
+
+btnElList.forEach(btnEl => {
+  btnEl.addEventListener('click', () => {
+    // Check if the clicked button already has the 'special' class
+    if (btnEl.classList.contains('special')) {
+      // If it does, remove the 'special' class (deselect it)
+      btnEl.classList.remove('special');
+    } else {
+      // Add the 'special' class to the clicked button (select it)
+      btnEl.classList.add('special');
+    }
+  });
+});
